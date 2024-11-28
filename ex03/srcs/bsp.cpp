0113f3c911ec5aa9e64804571af249159bf0326f
Elodie Briere <ebriere@z4r5p2.42lyon.fr>
2024-11-28 17:32:12 +0100

    bsp fonctionnelle

--- ex03/srcs/bsp.cpp
+++ ex03/srcs/bsp.cpp
@@ -1,43 +1,17 @@
 #include "Point.hpp"
 #include <iostream>
 
-using namespace std; //enlever
-
-static Fixed	getArea(Point const a, Point const b, Point const c)
-{
-		
-	cout << "A -> x = " <<a._x.getRawBits() << " y = " << a._y.getRawBits() << endl;
-	cout << "B -> x = " <<b._x.getRawBits() << " y = "  << b._y.getRawBits() << endl;
-	cout << "C -> x = " <<c._x.getRawBits() << " y = " << c._y.getRawBits() << "\n"<< endl;
-
-	Fixed Area = Fixed(0.5f) * (a.getFixed_x() * (b.getFixed_y() - c.getFixed_y())
-							+ b.getFixed_x() * (c.getFixed_y() - a.getFixed_y())
-							+ c.getFixed_x() * (a.getFixed_y() - b.getFixed_y()));
-
-//	Fixed Area = a.getFixed_x() / b.getFixed_x();
-//	std::cout << Area << std::endl; 
-	return (Area);
-}
 
 bool bsp(Point const a, Point const b, Point const c, Point const point)
 {
-	Fixed A1 = getArea (a, b ,c);
-	Fixed A2 = getArea (a, b, point);
-	Fixed A3 = getArea (a, point,c);
-	Fixed A4 = getArea (point, b ,c);
+	Fixed x = (b.getFixed_x() - a.getFixed_x()) * (point.getFixed_y() - a.getFixed_y())
+				- (b.getFixed_y() - a.getFixed_y()) * (point.getFixed_x() - a.getFixed_x());
+    Fixed y = (c.getFixed_x() - b.getFixed_x()) * (point.getFixed_y() - b.getFixed_y())
+				- (c.getFixed_y() - b.getFixed_y()) * (point.getFixed_x() - b.getFixed_x());
+    Fixed z = (a.getFixed_x() - c.getFixed_x()) * (point.getFixed_y() - c.getFixed_y())
+				- (a.getFixed_y() - c.getFixed_y()) * (point.getFixed_x() - c.getFixed_x());
 
-	Fixed total = A2 + A3 + A4;
-
-	std::cout << BLUE <<"Aire de ABC  = " << RESET << A1 << " | ";
-	std::cout << A1.getRawBits() << std::endl;
-
-	std::cout << PINK << "Aire total des 3 triangles  = " << RESET << total << " | ";
-	std::cout << total.getRawBits() << std::endl;
-
-
-	if (A2 == 0 || A3 == 0 || A4 == 0)
-		return (false);	
-	if (A2 + A3 + A4 == A1)
-		return (true);
-	return (false);
+    if ((x > 0 && y > 0 && z > 0) || (x < 0 && y < 0 && z < 0))
+        return (true);
+    return (false);
 }
--- ex03/srcs/main.cpp
+++ ex03/srcs/main.cpp
@@ -3,40 +3,41 @@
 
 int main (void) 
 {
-	/*Point	A(Fixed(5), Fixed(5));
+	/Point	A(Fixed(5), Fixed(5));
 	Point	B(Fixed(6), Fixed(3));
 	Point	C(Fixed(1), Fixed(2));
 	Point	point(Fixed(4), Fixed(3));
 
-	std::cout << "A : (x = " << A.getFixed_x() << ", y = " << A.getFixed_y() << ") "<< std::endl;
-	std::cout << "B : (x = " << B.getFixed_x() << ", y = " << B.getFixed_y() << ") "<< std::endl;
-	std::cout << "C : (x = " << C.getFixed_x() << ", y = " << C.getFixed_y() << ") "<< std::endl;
-	std::cout << "point : (x = " << point.getFixed_x() << ", y = " << point.getFixed_y() << ") \n"<< std::endl;
 	if (bsp(A, B, C, point) == true)
 		std::cout << "True : The point is in the triangle" << std::endl;
 	else
 		std::cout << "False : The point is not in the triangle" << std::endl;
-//	std::cout << (Fixed(5) * Fixed(2)) << std::endl;
-//	std::cout << (Fixed(0.5f) / Fixed(2)) << std::endl;*/
 //
 
 	//-----------------------------------------------------------------------//
 	
-	Point A(Fixed(2), Fixed(2));
+/*	Point A(Fixed(2), Fixed(2));
 	Point B(Fixed(2), Fixed(5));
 	Point C(Fixed(6), Fixed(3));
-	Point point (Fixed(2.2f), Fixed(4));
-
-	std::cout << "A : (x = " << A.getFixed_x() << ", y = " << A.getFixed_y() << ") "<< std::endl;
-	std::cout << "B : (x = " << B.getFixed_x() << ", y = " << B.getFixed_y() << ") "<< std::endl;
-	std::cout << "C : (x = " << C.getFixed_x() << ", y = " << C.getFixed_y() << ") "<< std::endl;
-	std::cout << "point : (x = " << point.getFixed_x() << ", y = " << point.getFixed_y() << ") \n"<< std::endl;
+	Point point (Fixed(2), Fixed(3.5f));
 
 	if (bsp(A, B, C, point) == true)
 		std::cout << "True : The point is in the triangle" << std::endl;
 	else
 		std::cout << "False : The point is not in the triangle" << std::endl;
+*/
 
+	//-----------------------------------------------------------------------/
 
+	/*Point A(Fixed(2), Fixed(2));
+	Point B(Fixed(2), Fixed(5));
+	Point C(Fixed(6), Fixed(3));
+	Point point (Fixed(6), Fixed(3));
+
+	if (bsp(A, B, C, point) == true)
+		std::cout << "True : The point is in the triangle" << std::endl;
+	else
+		std::cout << "False : The point is not in the triangle" << std::endl;
+	*/
 	return (0);
 } 
